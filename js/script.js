06a20220abba548bd5951a0fc5580a0860062e63
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-23 17:01:48 +0200

    Added: some experiments

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      //consegna
       message: 'hello word',
-      imgURL: 'https://picsum.photos/400/200'
+      imgURL: 'https://picsum.photos/500/300',
+
+      // esperimenti
+      titleClass: 'custom-title',
+      colors: ['bg-blue','bg-green','bg-aqua'],
+      num: 0,
+      imgBorders: 'b-rad-40'
     }
   },
+  // functions
+  methods: {
+    increaseValue() {
+      this.num++
+    },
+
+    resetCounter() {
+      this.num = 0
+    }
+  }
 }).mount('#app');
